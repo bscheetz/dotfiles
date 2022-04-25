6924088ec3c18a2bc8c4597661b7e537bc5a675d
Ben Scheetz <8943045+bscheetz@users.noreply.github.com>
2022-04-25 02:23:28 -0400

    Improved install of nodejs on ubuntu.

--- setup.sh
+++ setup.sh
@@ -65,9 +65,11 @@ install_packages () {
                 direnv \
                 ripgrep \
                 tmux \
-                kubectl \
-                nodejs \
-                npm
+                kubectl
+
+        # install node
+        curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+        sudo apt-get install -y nodejs
 
     elif [ "$(uname)" = "Darwin" ]; then
         brew install \
