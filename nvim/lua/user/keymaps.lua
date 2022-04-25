31ecb5f8f0b722bafc823fecd3298c4579379047
Ben Scheetz <8943045+bscheetz@users.noreply.github.com>
2022-04-24 21:38:00 -0400

    Improved keymaps for inter-pane movement.

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,16 +1,24 @@
 local opts = { noremap = true, silent = true }
 
+local keymap = vim.api.nvim_set_keymap
+
 vim.g.mapleader = " "
 -- map arrow keys in normal mode to shift pane edges
-vim.api.nvim_set_keymap("n", "<Left>", ":vertical resize -1<CR>", opts)
-vim.api.nvim_set_keymap("n", "<Right>", ":vertical resize +1<CR>", opts)
-vim.api.nvim_set_keymap("n", "<Up>", ":resize -1<CR>", opts)
-vim.api.nvim_set_keymap("n", "<Down>", ":resize +1<CR>", opts)
+keymap("n", "<Left>", ":vertical resize -1<CR>", opts)
+keymap("n", "<Right>", ":vertical resize +1<CR>", opts)
+keymap("n", "<Up>", ":resize -1<CR>", opts)
+keymap("n", "<Down>", ":resize +1<CR>", opts)
 
 -- return to the last file opened by pressing leader leader
-vim.api.nvim_set_keymap("n", "<Leader><Leader>", "<c-^>", opts)
+keymap("n", "<Leader><Leader>", "<c-^>", opts)
 
 -- press tab to switch to the next buffer
 -- press shift tab to switch to the previous buffer
-vim.api.nvim_set_keymap("n", "<Tab>", ":bnext!<CR>", opts)
-vim.api.nvim_set_keymap("n", "<S-Tab>", ":bprev!<CR>", opts)
+keymap("n", "<Tab>", ":bnext!<CR>", opts)
+keymap("n", "<S-Tab>", ":bprev!<CR>", opts)
+
+-- better movement between vim panes
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
