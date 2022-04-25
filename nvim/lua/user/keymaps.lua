44451d9ca2e10537c43b8bb1587cb7cc1171b7d3
Ben Scheetz <8943045+bscheetz@users.noreply.github.com>
2022-04-24 21:44:59 -0400

    Improved keymaps for inter-pane movement.

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -2,6 +2,15 @@ local opts = { noremap = true, silent = true }
 
 local keymap = vim.api.nvim_set_keymap
 
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 -- map arrow keys in normal mode to shift pane edges
 keymap("n", "<Left>", ":vertical resize -1<CR>", opts)
@@ -22,3 +31,12 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Select text in visual block mode and move it up/down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
