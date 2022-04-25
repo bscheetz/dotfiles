09f6368363057b08a74a21e4ebc3fd1fe7105a7b
Ben Scheetz <8943045+bscheetz@users.noreply.github.com>
2022-04-25 11:53:34 -0400

    Added autopairs.

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -44,6 +44,7 @@ return packer.startup(function(use)
     use "nvim-lua/popup.nvim"
     use "nvim-lua/plenary.nvim"
     use "akinsho/bufferline.nvim"
+    use "windwp/nvim-autopairs"
 
     use "nvim-lualine/lualine.nvim"
 
