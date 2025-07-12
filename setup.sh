998d18c0c713f1a27097e7bbe500a9b9a22948aa
bscheetz <8943045+bscheetz@users.noreply.github.com>
2025-07-12 13:12:04 -0400

    Switch to pylsp

--- setup.sh
+++ setup.sh
@@ -58,17 +58,17 @@ install_packages () {
                 python3-venv \
                 direnv \
                 ripgrep \
-                tmux \
-                clangd
+               	tmux \
+               	clangd
 
         # install node
         curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
         sudo apt-get install -y nodejs
 
-    	# setup for kubectl
-    	curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-    	sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-	echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+        # setup for kubectl
+	curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+        sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+        echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 	sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
 	sudo apt-get update
 	sudo apt install -y kubectl
@@ -186,7 +186,7 @@ setup_neovim () {
 
     nvim --headless +PlugInstall +qa
     nvim --headless +UpdateRemotePlugins +qa
-    nvim --headless +"LspInstall pyright" +qa
+    nvim --headless +"LspInstall pylsp" +qa
 
     pyenv deactivate
 }
