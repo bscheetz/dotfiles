8432427fe886256536e44898d968b5a85947ed23
bscheetz <8943045+bscheetz@users.noreply.github.com>
2025-07-14 10:46:52 -0400

    Fix none-ls using source for eslint. (#25)

--- nvim/lua/user/lsp/none-ls.lua
+++ nvim/lua/user/lsp/none-ls.lua
@@ -3,12 +3,23 @@ if not null_ls_status_ok then
     return
 end
 
+local sources = {
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.prettier,
+}
+
+-- prefer eslint_d if available, else fall back to eslint
+local eslint = null_ls.builtins.diagnostics.eslint_d
+if not eslint then
+    eslint = null_ls.builtins.diagnostics.eslint
+end
+
+if eslint then
+    table.insert(sources, eslint)
+end
+
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint,
-    },
+    sources = sources,
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
             vim.api.nvim_clear_autocmds({ group = "LspFormatting", buffer = bufnr })
