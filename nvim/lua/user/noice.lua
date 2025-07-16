e6003f8294e7dc4b5dfa42d1b8383088e3b766b9
Ben Scheetz <8943045+bscheetz@users.noreply.github.com>
2025-07-16 09:00:51 -0400

    Add noice and telescope-fzf. Remove fixed commit for telescope.

---
+++ nvim/lua/user/noice.lua
@@ -0,0 +1,22 @@
+require("noice").setup({
+    lsp = {
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
+    },
+  },
+  presets = {
+    bottom_search = true,
+    command_palette = true,
+    long_message_to_split = true,
+  },
+})
+
+vim.opt.cmdheight = 0
+
+-- Optional: use noice with notify
+require("notify").setup({
+  background_colour = "#000000",
+  stages = "fade",
+})
