b92655b19bacfbef0549fa9e5e3b7d31941ae491
Ben Scheetz <8943045+bscheetz@users.noreply.github.com>
2025-07-16 08:59:16 -0400

    Fix none-ls group definition.

--- nvim/lua/user/lsp/none-ls.lua
+++ nvim/lua/user/lsp/none-ls.lua
@@ -18,13 +18,15 @@ if eslint then
     table.insert(sources, eslint)
 end
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
     sources = sources,
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = "LspFormatting", buffer = bufnr })
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
             vim.api.nvim_create_autocmd("BufWritePre", {
-                group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
+                group = augroup,
                 buffer = bufnr,
                 callback = function()
                     vim.lsp.buf.format({ bufnr = bufnr })
