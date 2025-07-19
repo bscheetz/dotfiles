390ed1344c8f99c0d5c20c4fb8a79a8a6810d314
Ben Scheetz <8943045+bscheetz@users.noreply.github.com>
2025-07-19 11:42:31 -0400

    Fix mason-lspconfig setup.

--- nvim/lua/user/lsp/mason.lua
+++ nvim/lua/user/lsp/mason.lua
@@ -10,24 +10,31 @@ if not mason_lsp_status then
   return
 end
 
-local servers = { "sumneko_lua", "jsonls" }
+local servers = { "lua_ls", "jsonls" }
 
-mason_lspconfig.setup {
+mason_lspconfig.setup({
   ensure_installed = servers,
+})
+
+mason_lspconfig.setup({
+    ensure_installed = servers,
+    automatic_installation = false,
+    automatic_enable = true,
+})
+
+local lspconfig = require("lspconfig")
+local handler_opts = {
+    on_attach = require("user.lsp.handlers").on_attach,
+    capabilities = require("user.lsp.handlers").capabilities,
 }
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    local opts = {
-      on_attach = require("user.lsp.handlers").on_attach,
-      capabilities = require("user.lsp.handlers").capabilities,
-    }
+for _, server in ipairs(servers) do 
+    local opts = vim.tbl_deep_extend("force", {}, handler_opts)
 
-    local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings." .. server_name)
+    local has_custom_opts, server_custom_opts = pcall(require, "user.lsp.settings" .. server)
     if has_custom_opts then
-      opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
+        opts = vim.tbl_deep_extend("force", opts, server_custom_opts)
     end
 
-    require("lspconfig")[server_name].setup(opts)
-  end,
-}
+    lspconfig[server].setup(opts)
+end
