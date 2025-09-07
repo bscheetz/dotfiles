4d9fefe8e85321170a2221629354bf2e4cc6a491
Ben Scheetz <8943045+bscheetz@users.noreply.github.com>
2025-09-07 06:15:00 -0400

    Remove defaulting of lazy to "false".

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,76 +1,71 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 return require("lazy").setup({
-  "nvim-lua/popup.nvim",
-  "nvim-lua/plenary.nvim",
-  "akinsho/bufferline.nvim",
-  "windwp/nvim-autopairs",
-  "lewis6991/impatient.nvim",
+	"nvim-lua/popup.nvim",
+	"nvim-lua/plenary.nvim",
+	"akinsho/bufferline.nvim",
+	"windwp/nvim-autopairs",
+	"lewis6991/impatient.nvim",
 
-  "nvim-lualine/lualine.nvim",
+	"nvim-lualine/lualine.nvim",
 
-  -- cmp plugins
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "hrsh7th/cmp-cmdline",
-  "saadparwaiz1/cmp_luasnip",
-  "hrsh7th/cmp-nvim-lsp",
+	-- cmp plugins
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	"saadparwaiz1/cmp_luasnip",
+	"hrsh7th/cmp-nvim-lsp",
 
-  -- snippets
-  "L3MON4D3/LuaSnip",
-  "rafamadriz/friendly-snippets",
+	-- snippets
+	"L3MON4D3/LuaSnip",
+	"rafamadriz/friendly-snippets",
 
-  -- LSP
-  "neovim/nvim-lspconfig",
-  {
-    "williamboman/mason.nvim",
-    build = function()
-      pcall(vim.cmd, "MasonUpdate")
-    end,
-  },
-  "williamboman/mason-lspconfig.nvim",
-  "tamago324/nlsp-settings.nvim",
-  "nvimtools/none-ls.nvim",
-  {
-    "folke/noice.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
-    config = function()
-      require("user.noice")
-    end,
-  },
-
-  -- NvimTree
-  "kyazdani42/nvim-web-devicons",
-  "kyazdani42/nvim-tree.lua",
-  -- colorscheme
-  "LunarVim/darkplus.nvim",
-
-  "nvim-treesitter/nvim-treesitter",
-  -- Telescope
-  "nvim-telescope/telescope.nvim",
-  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-  "HiPhish/rainbow-delimiters.nvim",
-
-  "lewis6991/gitsigns.nvim",
-  "christoomey/vim-tmux-navigator",
+    -- LSP
+    "neovim/nvim-lspconfig",
+    {
+      "williamboman/mason.nvim",
+      build = function()
+        pcall(vim.cmd, "MasonUpdate")
+      end,
+    },
+    "williamboman/mason-lspconfig.nvim",
+    "tamago324/nlsp-settings.nvim",
+    "nvimtools/none-ls.nvim",
+    {
+      "folke/noice.nvim",
+      dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+      config = function()
+        require("user.noice")
+      end,
+    },
+    -- NvimTree
+    "kyazdani42/nvim-web-devicons",
+    "kyazdani42/nvim-tree.lua",
+    -- colorscheme
+    "LunarVim/darkplus.nvim",
+    
+    "nvim-treesitter/nvim-treesitter",
+    -- Telescope
+    "nvim-telescope/telescope.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "HiPhish/rainbow-delimiters.nvim",
+    
+    "lewis6991/gitsigns.nvim",
+    "christoomey/vim-tmux-navigator",
 }, {
-  ui = {
-    border = "rounded",
-  },
-  defaults = {
-    lazy = false,
-  },
+	ui = {
+		border = "rounded",
+	},
 })
-
