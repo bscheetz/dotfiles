85ea2d2c5d8ad9b4b1d2f2aae63cbd49c2071b8d
Ben Scheetz <8943045+bscheetz@users.noreply.github.com>
2025-09-07 06:07:59 -0400

    Reformat lua files.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,19 +1,25 @@
 vim.g.loaded_python_provider = 0
 vim.g.python3_host_prog = os.getenv("PYENV_ROOT") .. "/versions/neovim3/bin/python3"
+vim.env.PATH = table.concat({
+	vim.env.PATH,
+	vim.fn.stdpath("data") .. "/mason/bin",
+	vim.fn.expand("~/.local/bin"),
+	vim.fn.expand("~/.npm-global/bin"),
+}, ":")
 
-require "user.colors"
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.nvim-tree"
-require "user.lualine"
-require "user.gitsigns"
-require "user.bufferline"
+require("user.colors")
+require("user.options")
+require("user.keymaps")
+require("user.plugins")
+require("user.cmp")
+require("user.lsp")
+require("user.telescope")
+require("user.treesitter")
+require("user.nvim-tree")
+require("user.lualine")
+require("user.gitsigns")
+require("user.bufferline")
 
-vim.cmd 'filetype plugin on'
+vim.cmd("filetype plugin on")
 
-vim.cmd 'colorscheme darkplus'
+vim.cmd("colorscheme darkplus")
--- nvim/lua/user/lsp/none-ls.lua
+++ nvim/lua/user/lsp/none-ls.lua
@@ -1,37 +1,37 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-    return
+	return
 end
 
 local sources = {
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.prettier,
+	null_ls.builtins.formatting.stylua,
+	null_ls.builtins.formatting.prettier,
 }
 
 -- prefer eslint_d if available, else fall back to eslint
 local eslint = null_ls.builtins.diagnostics.eslint_d
 if not eslint then
-    eslint = null_ls.builtins.diagnostics.eslint
+	eslint = null_ls.builtins.diagnostics.eslint
 end
 
 if eslint then
-    table.insert(sources, eslint)
+	table.insert(sources, eslint)
 end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-    sources = sources,
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })
-                end,
-            })
-        end
-    end,
+	sources = sources,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
