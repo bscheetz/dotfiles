bd87754f89a2d2d9dc57ba941f9bd8dea5520c11
Ben Scheetz <8943045+bscheetz@users.noreply.github.com>
2025-11-04 10:30:13 -0500

    Update zsh configuration to include prompt icon. Remove duplication between main and other files.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,19 @@
+setopt correctall
+
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 alias gs='git status'
+alias ga='git add'
 alias gc='git commit'
 alias gp='git push'
 alias gl='git pull'
-alias v='nvim'
+
+alias make='nocorrect make'
+alias git='nocorrect git'
+alias rg='nocorrect rg'
+alias kb='kubectl'
+
+if type nvim > /dev/null 2>&1; then
+      alias vi='nvim'
+fi
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -2,3 +2,12 @@ export PATH="$HOME/bin:$PATH"
 export EDITOR="nvim"
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
+
+export XDG_CONFIG_HOME=$HOME/.config
+
+# Add local bin to path
+export PATH="$PATH:$HOME/.local/bin"
+# Add cargo to path
+export PATH=$PATH:/home/ben/.cargo/bin
+# Add npm global to path
+export PATH=~/.npm-global/bin:$PATH
