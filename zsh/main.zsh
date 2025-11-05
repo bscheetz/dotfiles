023bc1c24858b5d0ebdaf42594fa2201ec921b76
bscheetz <8943045+bscheetz@users.noreply.github.com>
2025-11-05 09:58:47 -0500

    Fix zsh and install uv (#35)
    * Update zsh configuration to include prompt icon. Remove duplication between main and other files.
    
    * Update PATH set by zshrc to include $HOME/.local/.
    
    * Remove setting of python3_host_prog in nvim/init.lua.
    
    * Install uv and pynvim into uv tools.

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -7,22 +7,6 @@ precmd() { print "" }
 PS1="⟩"
 RPS1="%{$fg[magenta]%}%20<...<%~%<<%{$reset_color%}"
 
-plugins=(git docker docker-compose)
-
-alias_nvim () {
-      if type nvim > /dev/null 2>&1; then
-            alias vi='nvim'
-      fi
-}
-
-set_up_antigen () {
-      # ZSH package manager
-      if [ $commands[kubectl] ]; then source <(kubectl completion zsh); fi # kubectl autocomplete
-
-      source ~/antigen.zsh
-      antigen init ~/.antigenrc
-}
-
 execute_tmux () {
       # autostart tmux if it exists
       if [ "$TMUX" = "" ]
@@ -37,10 +21,6 @@ execute_tmux () {
       fi
 }
 
-set_xdg_config_path () {
-      export XDG_CONFIG_HOME=$HOME/.config
-}
-
 set_up_direnv () {
       # Enable direnv
       eval "$(direnv hook zsh)"
@@ -50,22 +30,6 @@ set_up_fzf () {
       [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 }
 
-set_up_pyenv () {
-      export PYENV_ROOT="$HOME/.pyenv"
-      export PATH="$PYENV_ROOT/bin:$PYENV_ROOT/shims:$PATH"
-
-      eval "$(pyenv init -)"
-      eval "$(pyenv virtualenv-init -)"
-}
-
-add_poetry_to_path () {
-      export PATH="$HOME/.poetry/bin:$PATH"
-}
-
-add_local_bin_to_path () {
-      export PATH="$PATH:$HOME/.local/bin"
-}
-
 set_up_cross_distro_mounts_wsl () {
       if [[ ! -z "${WSL_DISTRO_NAME+x}" && ! -d /mnt/wsl/home ]]; then
             # if WSL_DISTRO_NAME is set and the /mnt/wsl/home dir doesn't exist
@@ -73,10 +37,6 @@ set_up_cross_distro_mounts_wsl () {
             mkdir /mnt/wsl/home
             sudo mount --bind ~ /mnt/wsl/home
       fi
-}
-
-add_vue_cli_to_path () {
-      export PATH=~/.npm-global/bin:$PATH
 }
 
 set_up_cuda () {
@@ -98,36 +58,17 @@ set_up_autocomplete () {
     source <(docker completion zsh)
 }
 
-add_cargo_to_path () {
-    export PATH=$PATH:/home/ben/.cargo/bin
-}
-
 # entering a directory automatically changes to that directory
 setopt auto_cd
 
-set_up_antigen
-
 # enable vim mode
 bindkey -v
-# correct typos. ignore `git status` because there is some glitch
-setopt correctall
-alias make='nocorrect make'
-alias git='nocorrect git'
-alias rg='nocorrect rg'
-alias kb='kubectl'
 
-alias_nvim
-set_xdg_config_path
 set_up_direnv
 set_up_cross_distro_mounts_wsl
-add_poetry_to_path
-add_local_bin_to_path
-add_vue_cli_to_path
-set_up_pyenv
 set_up_fzf
 set_up_cuda
 set_up_nvm
 set_up_autocomplete
-add_cargo_to_path
 
 execute_tmux
